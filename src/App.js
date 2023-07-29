eced32f4ead5752a6afdd5ce3c5d1ce818b03ab8
unknown <lgh7956@gmail.com>
2023-07-29 22:36:38 +0900

    가힇

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 
-import Category from './pages_Category/Category';
-
 import { GlobalStyle } from "./GlobalStyle";
 import { Link } from "react-router-dom";
 import App_methods from "./pages_app_methods/App_methods";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FontSizeSetting from "./pagesHongseok/FontSizeSetting";
+import StartPage from "./pagesHongseok/StartPage";
+import Test from "./pages_Test/Test";
+import Home from "./pages_home/Home";
+import Category from "./pages_Category/Category";
 
 
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <Category/>
-<App_methods></App_methods>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/fontsizeSetting" element={<FontSizeSetting />} />
+          <Route path="/Test" element={<Test/>}/>
+          <Route path="/Main" element={<Home/>}/>
+          <Route path="/Category" element={<Category/>}/>
+    
+
+        </Routes>
+      </BrowserRouter>
+
     </>
-   
   );
 }
 
