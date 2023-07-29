b7023fcdbbf8a04253a7264917e38b9c0c126518
hyori Choi <hyoring030@gmail.com>
2023-07-29 17:27:03 +0900

    휴

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import App_methods from "./pages_app_methods/App_methods";
+import { GlobalStyle } from "./GlobalStyle";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle/>
+<App_methods></App_methods>
+    </>
+   
   );
 }
 
