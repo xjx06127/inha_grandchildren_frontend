814ee4fbd0a40d17918e73f23d3e88dc3e18e9a9
taek <yeontaek94@naver.com>
2023-08-07 18:14:54 +0900

    alert styled 기능 추가

--- src/pages_home/HomeNavigator.jsx
+++ src/pages_home/HomeNavigator.jsx
@@ -1,58 +1,88 @@
-import React from 'react';
+import React, { useContext } from "react";
 import styled from "styled-components";
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FontSizeContext } from "../pages_font_context/FontSizeProvider";
+import Swal from "sweetalert2";
 
 const Box = styled.div`
-    display: flex;
-    justify-content: end;
-    padding-left: 4.8%;
-    padding-right: 4.8%;
-    padding-top: 7%;
-`
+  display: flex;
+  justify-content: end;
+  padding-left: 4.8%;
+  padding-right: 4.8%;
+  padding-top: 7%;
+`;
 const SoundBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const SoundImg = styled.img`
-   width: 40%;
-   @media (orientation: portrait) {
-    /* Set the height specifically for portrait mode */
-    height: 3vh;
+  width: 40%;
+  height: 40%;
+
+  margin-bottom: 7px;
+`;
+
+const SoundText = styled.p`
+  color: white;
+  /* font-size: 1rem; */
+  font-size: ${(props) => {
+    switch (props.fS) {
+      case "normal":
+        return "1rem";
+      case "large":
+        return "1.3rem";
+      case "veryLarge":
+        return "1.6rem";
     }
-   margin-bottom: 7px;
+  }};
 `;
-
-const SoundText = styled.p`  
-    color: white;
-    font-size: 1rem;
-`;
-
 
 const HomeNavigator = () => {
-    const [isSoundOffClicked,setSoundOffClicked] = useState(false);
-    
-    const handleControlSound = () => {
-        setSoundOffClicked(!isSoundOffClicked);
-    }
+  const [isSoundOffClicked, setSoundOffClicked] = useState(false);
+  const { fontSize, setFontSize } = useContext(FontSizeContext);
 
-    return (
-       <>
-       <Box>
-       <SoundBox>
-        <SoundImg 
-        src={isSoundOffClicked ? '/soundoff_white.svg' : '/sound.svg'}
-        onClick={handleControlSound}/>
-        <SoundText>{
-            isSoundOffClicked ? '소리 켜기' : '소리 끄기'
-            }</SoundText>
-       </SoundBox>
-       </Box>
+  const handleControlSound = () => {
+    setSoundOffClicked(!isSoundOffClicked);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "top",
+      showConfirmButton: false,
+      timer: 1200,
+      timerProgressBar: false,
+      didOpen: (toast) => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+      },
+    });
+    isSoundOffClicked === true
+      ? Toast.fire({
+          icon: "success",
+          title: "음성 지원 소리를 켰습니다.",
+        })
+      : Toast.fire({
+          icon: "success",
+          title: "음성 지원 소리를 껐습니다.",
+        });
+  };
 
-       </>
-    );
+  return (
+    <>
+      <Box>
+        <SoundBox>
+          <SoundImg
+            src={isSoundOffClicked ? "/soundoff_white.svg" : "/sound.svg"}
+            onClick={handleControlSound}
+          />
+          <SoundText fS={fontSize}>
+            {isSoundOffClicked ? "소리 켜기" : "소리 끄기"}
+          </SoundText>
+        </SoundBox>
+      </Box>
+    </>
+  );
 };
 
 export default HomeNavigator;
