70fd6a5feb534a9dac75f5cefbac4e16e014a9f4
HongseokChoi <xjx06127@inha.edu>
2023-08-09 21:56:44 +0900

    합침

--- src/pages_AppPage/TestAppPage.jsx
+++ src/pages_AppPage/TestAppPage.jsx
@@ -515,20 +515,18 @@ const TestAppPage = () => {
 
     //안드로이드 기종인 경우
     if (mobileType.indexOf("android") > -1) {
-      //Googleplay스토어에 있는 경우
-      if (AndroidUrl) {
-        return window.open(AndroidUrl);
-      }
-      else
       //Googleplay스토어에 없는 경우
-      Swal.fire({
-        icon: "error",
-        title: "잠시만요!",
-        text: "해당 어플은 현재 단말기에서 지원하지 않아요.",
-        confirmButtonText: "다른 어플 보러가기",
-        confirmButtonColor: "#798560",
-      });
-      return;
+      if (AndroidUrl === null) {
+        Swal.fire({
+          icon: "error",
+          title: "잠시만요!",
+          text: "해당 어플은 현재 단말기에서 지원하지 않아요.",
+          confirmButtonText: "다른 어플 보러가기",
+          confirmButtonColor: "#798560",
+        });
+      }
+      //Googleplay스토어에 있는 경우
+      return window.open(AndroidUrl);
     }
 
     //애플 기종인 경우
@@ -537,27 +535,22 @@ const TestAppPage = () => {
       mobileType.indexOf("ipad") > -1 ||
       mobileType.indexOf("ipod") > -1
     ) {
-      if (iosUrl) {
-        window.open(iosUrl);
-        return;
+      if (iosUrl === null) {
+        Swal.fire({
+          icon: "error",
+          title: "잠시만요!",
+          text: "해당 어플은 현재 단말기에서 지원하지 않아요.",
+          confirmButtonText: "다른 어플 보러가기",
+          confirmButtonColor: "#798560",
+        });
       }
-      else
-      Swal.fire({
-        icon: "error",
-        title: "잠시만요!",
-        text: "해당 어플은 현재 단말기에서 지원하지 않아요.",
-        confirmButtonText: "다른 어플 보러가기",
-        confirmButtonColor: "#798560",
-      });
-      return;
+      return window.open(iosUrl);
     }
-
     //현재 데스크톱인 경우
     else if (isMac) {
       //Mac인경우
       window.open(iosUrl);
-    } 
-    else window.open(AndroidUrl); //Mac이 아닌 경우
+    } else window.open(AndroidUrl); //Mac이 아닌 경우
   };
 
   const handleButtonClick2 = async () => {
@@ -660,13 +653,6 @@ const TestAppPage = () => {
           <TtsBtn onClick={() => tts()}>
             <TtsImg />
           </TtsBtn>
-          {showToolTip && (
-            <ToolTip>
-              <ToolTipText>
-                Click to listen to the app's description
-              </ToolTipText>
-            </ToolTip>
-          )}
         </FixBox>
       </Desktop>
     </>
