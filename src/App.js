11b061f86c257c9067aa385c4d576045b22a4f56
hyori Choi <hyoring030@gmail.com>
2023-08-09 21:35:49 +0900

    연택오빠 FindAnalyze야

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ import HealthCare from "./pages_app_find_test/healthy/HealthCare";
 import Treatment from "./pages_app_find_test/healthy/Treatment";
 import CommunityAppTest from "./pages_app_find_test/community/CommunityAppTest";
 import TestAppPage from "./pages_AppPage/TestAppPage";
+import FindAnalyze from "./pages_analyze/FindAnalyze";
 
 function App() {
   return (
@@ -79,24 +80,24 @@ function App() {
             <Route path="/level4" element={<Level4Reco />} />
             <Route path="/level5" element={<Level5Reco />} />
 
-            <Route path="/AppFind" element={<FirstChoosePage/>}/>
-            <Route path="/AppsFindResult/:appfield" element={<AppsFind/>}/>
+            <Route path="/AppFind" element={<FirstChoosePage />} />
+            <Route path="/AppsFindResult/:appfield" element={<AppsFind />} />
 
-            <Route path="/LivingAppsFind1" element={<LivingAppTest/>}/>
-            <Route path="/FinanceAppsFind" element={<Finance/>}/>
-            <Route path="/SearchLoadsAppsFind" element={<SearchLoad/>}/>
-            <Route path="/DeliveryAppsFind" element={<Delivery/>}/>
+            <Route path="/LivingAppsFind1" element={<LivingAppTest />} />
+            <Route path="/FinanceAppsFind" element={<Finance />} />
+            <Route path="/SearchLoadsAppsFind" element={<SearchLoad />} />
+            <Route path="/DeliveryAppsFind" element={<Delivery />} />
 
-            <Route path="/HobbyAppTest1" element={<HobbyAppTest/>}/>
-            <Route path="/IndoorAppsFind" element={<Indoor/>}/>
-            <Route path="/OutdoorAppsFind" element={<Outdoor/>}/>
+            <Route path="/HobbyAppTest1" element={<HobbyAppTest />} />
+            <Route path="/IndoorAppsFind" element={<Indoor />} />
+            <Route path="/OutdoorAppsFind" element={<Outdoor />} />
 
-            <Route path="/HealthyAppTest1" element={<HealthyAppTest/>}/>
-            <Route path="/HealthCareAppsFind" element={<HealthCare/>}/>
-            <Route path="/TreatmentAppsFind" element={<Treatment/>}/>
+            <Route path="/HealthyAppTest1" element={<HealthyAppTest />} />
+            <Route path="/HealthCareAppsFind" element={<HealthCare />} />
+            <Route path="/TreatmentAppsFind" element={<Treatment />} />
 
-            <Route path="/CommunityAppsFind" element={<CommunityAppTest/>}/>
-
+            <Route path="/CommunityAppsFind" element={<CommunityAppTest />} />
+            <Route path="/FindAnalyze" element={<FindAnalyze />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
