aa8a8b6f806455479e04d162b45c553c9b8c1bc7
taek <yeontaek94@naver.com>
2023-08-11 18:35:41 +0900

    home에서 appfind페이지까지 sound수정

--- src/pages_app_find_test/living/Finance.jsx
+++ src/pages_app_find_test/living/Finance.jsx
@@ -4,6 +4,7 @@ import GrayNavigator from "../GrayNavigator";
 import { useNavigate } from "react-router";
 import { useState, useEffect } from "react";
 import { FontSizeContext } from "../../pages_font_context/FontSizeProvider";
+import UnderNavigator from "../UnderNavigator";
 
 const BackGround = styled.div`
   background-color: #f7f7f7;
@@ -199,6 +200,7 @@ const Finance = () => {
             </Box>
           </Row2>
         </SelectBox>
+        <UnderNavigator/>
       </BackGround>
     </>
   );
