1be0b472e255382a33df1d1f1bae0165572a7ec3
hyori Choi <hyoring030@gmail.com>
2023-08-16 15:08:44 +0900

    ccc

--- src/pages_analyze/Analyze.jsx
+++ src/pages_analyze/Analyze.jsx
@@ -111,17 +111,17 @@ const Analyze = ({ percentage }) => {
 
   const navigate = useNavigate(); // navigate 함수 생성
 
-  // useEffect(() => {
-  //   const progressOffset = ((100 - percentage) / 100) * 502;
-  //   setOffset(progressOffset);
+  useEffect(() => {
+    const progressOffset = ((100 - percentage) / 100) * 502;
+    setOffset(progressOffset);
 
-  //   const timer = setTimeout(() => {
-  //     navigate(`/${OX}/${OX2}/${OX3}/${OX4}/${OX5}/Result`);
-  //   }, 3000);
+    const timer = setTimeout(() => {
+      navigate(`/${OX}/${OX2}/${OX3}/${OX4}/${OX5}/Result`);
+    }, 3000);
 
-  //   // 컴포넌트가 unmount 될 때 타이머 해제
-  //   return () => clearTimeout(timer);
-  // }, [percentage, navigate]);
+    // 컴포넌트가 unmount 될 때 타이머 해제
+    return () => clearTimeout(timer);
+  }, [percentage, navigate]);
 
   const { fontSize, setFontSize } = useContext(FontSizeContext);
   document.body.style = "background: white;";
